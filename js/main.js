cd3141c842a8a5002f652091a4466e54e723c401
Andriy <41745061+AndriyShkrobut@users.noreply.github.com>
2019-01-07 00:15:49 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
 $(document).ready(function(){
-    $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
-        var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
-        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
-            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1000); // анимируем скроолинг к элементу scroll_el
+    $('.go_to').click( function(){
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length != 0) {
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1000);
         }
-        return false; // выключаем стандартное действие
+        return false;
     });
 });
-// var counter = 0;
-// function clicked(element){
-//     counter++;
-//     element.innerHTML = "You've clicked " + counter + " times";
-// }
