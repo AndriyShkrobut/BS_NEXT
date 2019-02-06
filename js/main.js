5cb6c2f6f713814100a338dc3447d19e8d787eb9
Andriy <41745061+AndriyShkrobut@users.noreply.github.com>
2019-02-07 00:13:33 +0200

    Delete main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.go_to').click( function(){
-        var scroll_el = $(this).attr('href');
-        if ($(scroll_el).length != 0) {
-            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1000);
-        }
-        return false;
-    });
-});
